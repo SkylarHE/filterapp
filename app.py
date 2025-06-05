371cff6e7928c3bd94d5c4e7874e7695a209e352
skylarHE <skylarhe0302@163.com>
2025-06-05 14:10:54 +0800

    brighten filter

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def apply_filter(img, filter_name):
     img = img.convert("RGB")
     if filter_name == "Brighten":
         enhancer = ImageEnhance.Brightness(img)
-        return enhancer.enhance(1.3)
+        return enhancer.enhance(1.1)
     elif filter_name == "Cool":
         r, g, b = img.split()
         b = b.point(lambda i: min(255, i + 30))
